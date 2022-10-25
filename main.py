ce7696eb34e262c3389cf84e8cff69dcbdfac86e
HeresG <gabiheres@yahoo.com>
2022-10-25 12:28:08 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,59 @@
+def suma():
+    # Use a breakpoint in the code line below to debug your script.
+    print("Suma")
+    print("Introduceti primul numar de la tastatura(a)")
+    a = input()
+    print("a=",a)
+    print ("Introduceti al doilea termen de la tastura(b)")
+    b = input()
+    print("b=",b)
+    print("a+b=", int(a)+int(b))
+
+def diferanta():
+    # Use a breakpoint in the code line below to debug your script.
+    print("diferenta")
+    print("Introduceti primul numar de la tastatura(a)")
+    a = input()
+    print("a=",a)
+    print ("Introduceti al doilea termen de la tastura(b)")
+    b = input()
+    print("b=",b)
+    print("a-b=", int(a)-int(b))
+
+
+def perimetru():
+    # Use a breakpoint in the code line below to debug your script.
+    print("perimetru")
+    print("Introduceti prima latura(a)")
+    a = input()
+    print("a=",a)
+    print ("Introduceti a doua latura(b)")
+    b = input()
+    print("b=",b)
+    print("Introduceti a treia latura(c)")
+    c = input()
+    print("c=",c)
+    print("a+b+c=", int(a)+int(b)+int(c))
+
+def aria():
+    # Use a breakpoint in the code line below to debug your script.
+    print("aria")
+    print("Introduceti o latura(a)")
+    a = input()
+    print("a=",a)
+    print ("Introduceti a doua latura(b)")
+    b = input()
+    print("b=",b)
+    print("a*b=", int(a)*int(b))
+
+
+
+if __name__ == '__main__':
+    suma()
+    diferanta()
+    perimetru()
+    aria()
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
