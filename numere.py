8de232e0cd54132f7e15fde412fcd591bedb57c3
HeresG <gabiheres@yahoo.com>
2022-11-08 14:54:58 +0200

    11.08.2022

--- numere.py
+++ numere.py
@@ -81,6 +81,53 @@ def animale():
     g = 2 * p
     print("Numarul animalelor din curte este: ", c+p+g)
 
+def marte():
+    print("m:")
+    m = int(input())
+    print("n:")
+    n = int(input())
+    print("p:")
+    p = int(input())
+    print("o valoare pt a:")
+    a = int(input())
+    print("o valoare pt b:")
+    b = int(input())
+    print("o valoare pt c:")
+    c = int(input())
+    moneda_1 = a * m
+    moneda_2 = b * n
+    moneda_3 = c * p
+    print("Martianul Iggle are: ", moneda_1+ moneda_2 + moneda_3 , "lei martieni")
+
+def cumparaturi():
+    print("Va rog introduceti un pretul unei cutii de bomboane:")
+    B = int(input())
+    print("Va rog introduceti suma pe care o are Gigel")
+    S = int(input())
+    c = S//B
+    bo = (c+1) * B - S
+    print("Gigel poate cumpara:",c , "cutii de bomboane" , "iar pentru a mai cumpara o cutie are nevoie de" , bo , "lei.")
+
+
+def inaltimetriunghi():
+    print("Introduceti va rog lungimea laturii 1:")
+    l1 = int(input())
+    print("introduceti va rog lungimea laturii 2:")
+    l2 = int(input())
+    print("Introduceti va rog lungimea ipotenuzei:")
+    i = int(input())
+    h = l1 * l2 // i
+    print(" Lungimea inaltimii dusa din unghiul drept pe ipotenuza este:" , h , ".")
+
+def sumacifrapermutare():
+    print("Introduceti va rog un numar de 3 cifre distincte:")
+    n = int(input())
+    a = n % 10
+    b = n // 100
+    c = (n // 10) % 10
+    S = a*100+b*10+c+a*100+c*10+b+b*100+a*10+c+b*100+c*10+a+c*100+a*10+b+c*100+b*10+a
+    print(" Suma tuturor numerelor care se pot obtine prin interschimbarea cifrelor numarului cerut este:" , S , ".")
+
 
 if __name__ == "__main__":
     adunare2()
@@ -93,3 +140,7 @@ if __name__ == "__main__":
     globuri()
     gauss()
     animale()
+    marte()
+    cumparaturi()
+    inaltimetriunghi()
+    sumacifrapermutare()
